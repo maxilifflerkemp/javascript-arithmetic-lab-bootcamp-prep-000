5da6a82c8e6cc474f93737dd58f769f741349df8
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-23 21:08:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ function divide() {}
 
 function increment(n) { return n += 1 }
 function decrement(n) { return n -= 1 }
+
+function makeInt(string) { parseInt(string) }
+function preserveDecimal(string) { parseFloat(string) } 
